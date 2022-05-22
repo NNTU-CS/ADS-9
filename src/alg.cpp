3c6af02c6e127dda3da565e2987df0a5294d6d58
Maxim <90348056+rawreflect@users.noreply.github.com>
2022-05-22 17:18:30 +0300

    Grishin Maxim 21IST-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,28 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <string>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> bst;
+  std::ifstream file(filename);
+  std::string word = "";
+  char lwcase = 'a' - 'A';
+  if (!file.is_open()) {
+    throw std::string("Error! the file was not opened.");
+  }
+  while (!file.eof()) {
+    char temp = file.get();
+    if ((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z')) {
+      if (temp >= 'A' && temp <= 'Z')
+        temp += lwcase;
+      word += temp;
+    } else if (word != "") {
+      bst.add(word);
+      word = "";
+    }
+  }
+  file.close();
+  return bst;
 }
