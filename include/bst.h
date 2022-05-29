9870d0a3b7172301b19a0767ff6416e3bf2fad0d
EgorSporyshev <94712052+EgorSporyshev@users.noreply.github.com>
2022-05-29 16:52:46 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -16,40 +16,40 @@ class BST {
     int depth();
     int search(T);
  private:
-    Node* koren;
+    Node* root;
     Node* addNode(Node, T);
     int depthTree(Node);
     int searchNode(Node, T);
 };
 
 template <typename T>
-BST<T>::BST(): koren(nullptr) {}
+BST<T>::BST(): root(nullptr) {}
 
 template <typename T>
-typename BST<T>::Node BST<T>::addNode(Node* koren, T value) {
-    if (koren == nullptr) {
-        koren = new Node;
-        koren->value = value;
-        koren->count = 1;
-        koren->lt = koren->rt = nullptr;
-    } else if (koren->value > value) {
-        koren->lt = addNode(koren->lt, value);
-    } else if (koren->value < value) {
-        koren->rt = addNode(koren->rt, value);
+typename BST<T>::Node BST<T>::addNode(Node* root, T value) {
+    if (root == nullptr) {
+        root = new Node;
+        root->value = value;
+        root->count = 1;
+        root->lt = root->rt = nullptr;
+    } else if (root->value > value) {
+        root->lt = addNode(root->lt, value);
+    } else if (root->value < value) {
+        root->rt = addNode(root->rt, value);
     } else {
-        koren->count++;
+        root->count++;
     }
-    return koren;
+    return root;
 }
 
 template <typename T>
-int BST<T>::depthTree(Node* koren) {
-    if (koren == nullptr) {
+int BST<T>::depthTree(Node* root) {
+    if (root == nullptr) {
         return 0;
     } else {
         int left, right;
-        left = depthTree(koren->lt);
-        right = depthTree(koren->rt);
+        left = depthTree(root->lt);
+        right = depthTree(root->rt);
         if (right > left) {
             return right + 1;
         } else {
@@ -59,31 +59,31 @@ int BST<T>::depthTree(Node* koren) {
 }
 
 template <typename T>
-int BST<T>::searchNode(Node* koren, T val) {
-    if (koren == nullptr) {
+int BST<T>::searchNode(Node* root, T val) {
+    if (root == nullptr) {
         return 0;
-    } else if (koren->value == val) {
-        return koren->count;
-    } else if (koren->value > val) {
-        return searchNode(koren->lt, val);
+    } else if (root->value == val) {
+        return root->count;
+    } else if (root->value > val) {
+        return searchNode(root->lt, val);
     } else {
-        return searchNode(koren->rt, val);
+        return searchNode(root->rt, val);
     }
 }
 
 template <typename T>
 void BST<T>::add(T value) {
-    koren = addNode(koren, value);
+    root = addNode(root, value);
 }
 
 template <typename T>
 int BST<T>::depth() {
-    return depthTree(koren) - 1;
+    return depthTree(root) - 1;
 }
 
 template <typename T>
 int BST<T>::search(T value) {
-    return searchNode(koren, value);
+    return searchNode(root, value);
 }
 
 #endif  // INCLUDE_BST_H_
