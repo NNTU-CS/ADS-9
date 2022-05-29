e1b496f0cd3a6f1c67c09937310b0e54c5558e8d
EgorSporyshev <94712052+EgorSporyshev@users.noreply.github.com>
2022-05-29 22:52:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,26 +6,30 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  BST<std::string> TR;
-    std::ifstream file(filename);
-    if (!file) {
-        std::cout << "File error!" << std::endl;
-        return TR;
-    }
-    char e;
-    std::string mr = "";
-    while (!file.eof()) {
-        e = file.get();
-        if (e >= 'A' && e <= 'Z') {
-            e = e + ('a' - 'A');
-        }
-        if (e >= 'a' && e <= 'z') {
-            mr = mr + e;
-        } else {
-            TR.add(mr);
-            mr = "";
-        }
-    }
-    file.close();
-    return TR;
+  BST<std::string> MyTree;
+  std::ifstream fin(filename);
+  char i;
+  std::string wd = "";
+  if (!fin.is_open()) {
+      std::cout << "Error! File can't be open!" << std::endl;
+      return MyTree;
+  }
+  while (!fin.eof()) {
+      i = fin.get();
+      if (i >= 'A') {
+          if (i <= 'Z') {
+              i = i + 32;
+          }
+      }
+      if (i >= 'a') {
+          if (i <= 'z') {
+              wd += i;
+          }
+      } else {
+          MyTree.Add(wd);
+          wd = "";
+      }
+  }
+  fin.close();
+  return MyTree;
 }
