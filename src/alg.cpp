9c7ef46b0950006ca419f572d3ad8c0955aed475
TsvetyaevSergey <99178429+TsvetyaevSergey@users.noreply.github.com>
2022-05-29 17:37:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,7 +10,7 @@ using namespace std;
 BST<string> makeTree(const char* filename) {
   BST<string> Tree;
   ifstream file(filename);
-  if (!file.open()) {
+  if (!file) {
     throw string("File search error");
   }
   string slovo = "";
@@ -18,8 +18,7 @@ BST<string> makeTree(const char* filename) {
     char chr = file.get();
     int sim = chr;
     if ((sim >= 65 && sim <= 90) || (sim >= 97 && sim <= 122)) {
-      if (sim >=65 && sim <= 90)
-      {
+      if (sim >=65 && sim <= 90) {
         sim -= 32;
       }
       slovo += sim;
