56e1495e268437f5df3919b73b379b31c9abb423
Zhilin21-IST-4 <94217280+Zhilin21-IST-4@users.noreply.github.com>
2022-06-09 18:55:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,24 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> Tree;
+  std::string word = "";
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+    return Tree;
+  }
+  while (!file.eof()) {
+    char chan = file.get();
+    if ((chan >= 'A' && chan <= 'Z') || (chan >= 'a' && chan <= 'z')) {
+      if (chan >= 'A' && chan <= 'Z')
+        chan = tolower(chan);
+      word += chan;
+    } else {
+      Tree.add(word);
+      word = "";
+    }
+  }
+  file.close();
+  return Tree;
 }
