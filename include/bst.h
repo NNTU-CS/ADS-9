ec6c2c47db7cb9e468918b875df83c7c39b4902b
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-05-06 23:32:44 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,23 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+template <typename T>
+struct Node {
+    T value;
+    Node* left;
+    Node* right;
+};
+
+template <typename T>
+Node<T>* makeTree(const char* filename);
+
+template <typename T>
+void insert(Node<T>*& root, Node<T>* node);
+
+template <typename T>
+Node<T>* search(Node<T>* root, const std::string& word);
+
+template <typename T>
+void printTree(Node<T>* root);
+
 #endif  // INCLUDE_BST_H_
