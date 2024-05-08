b23a70becd146f8a3f030bd0fd5cbd80626b6078
platna <160319937+platna@users.noreply.github.com>
2024-05-08 19:21:34 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,28 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <string>
 
+template <typename T>
+class BST {
+public:
+    struct Node {
+        T data;
+        int count;
+        Node* left;
+        Node* right;
+
+        Node(const T& data) : data(data), count(1), left(nullptr), right(nullptr) {}
+    };
+
+    BST() : root(nullptr) {}
+    ~BST() { clear(root); }
+
+    void insert(const T& data);
+    int find(const T& data) const;
+    void clear(Node* node);
+
+private:
+    Node* root;
+};
 #endif  // INCLUDE_BST_H_
