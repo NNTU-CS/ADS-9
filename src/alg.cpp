73650c183416566e0d3432dea4634686549ddf25
platna <160319937+platna@users.noreply.github.com>
2024-05-08 19:22:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,27 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> tree;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "Ошибка открытия файла!" << std::endl;
+        return tree;
+    }
+
+    std::string word;
+    char ch;
+    while (file.get(ch)) {
+        if (std::isalpha(ch)) {
+            word += std::tolower(ch);
+        } else if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
+        }
+    }
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+    return tree;
 }
