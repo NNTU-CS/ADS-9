e079cd6d1fc6bcb1af1363a3c66aa29a4fdbd625
flapik19 <selivanovvlad3211@gmail.com>
2024-05-11 00:14:15 +0300

    ads 9

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,49 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
-#endif  // INCLUDE_BST_H_
+template <typename T> class BST {
+ private:
+   struct Node {
+    Node *left;
+    Node *right;
+    T data;
+    int count;
+    Node (const T& data) : data (data), count (1), left(nullptr), right (nullptr) {}
+  };
+
+  Node* root;
+
+  void add (Node*& node, const T& data) {
+    if (node == nullptr) {
+        node = new Node (data);
+    } else if (data < node->data) {
+        add(node->left, data);
+    } else if (data > node->data) {
+        add(node->rightm, data);
+    } else {
+        node->count++;
+    }
+  }
+
+  void printInOrder (Node* node) {
+    if (node != nullptr) {
+        printInOrder(node->left);
+        std::cout << node->data << node->count << std::endl;
+        printInOrder(node->right);
+    } 
+  } 
+ public:
+  BST() : root (nullptr) {}
+
+  void add (const T& data) {
+    add(root, data);
+  }
+
+  void printInOrder() {
+    printInOrder(root);
+  }
+};
+
+#endif // INCLUDE_BST_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,27 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <sstream>
+#include <string>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::ifstream file(filename);
+  if(file.is_open()) {
+    std::string word;
+    while (file >> word) {
+        std::stringstream ss;
+        for(char c : word) {
+            if (std::isalpha(c)) {
+                ss << (char)std::tolower(c);
+            }
+        }
+        if (ss.str().empty()) {
+            tree.add(ss.str());
+        }
+    }
+    file.close();
+  }
+  return tree;
 }
