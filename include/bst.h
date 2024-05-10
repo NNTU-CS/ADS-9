7c2612b8a7f3a3a4bbb1459f8e4e70d602604855
anchovy31 <146199120+anchovy31@users.noreply.github.com>
2024-05-10 14:59:49 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,27 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+template<typename T>
+class BST {
+	struct Node {
+		T value;
+		int count;
+		Node* left;
+		Node* right;
+	};
+	Node* root;
+	Node* addNode(Node* root,T value);
+	int depthTree(Node*);
+	int searchTree(Node*,T value);
+	void delTree(Node*);
+	void printTree(Node*);
 
+ public:
+	BST();
+	~BST();
+	void add(T value);
+	void print();
+	int depth();
+	int search(T value);
+};
 #endif  // INCLUDE_BST_H_
