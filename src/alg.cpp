75a2aa0e5b79b294079ae63357859af2d6101c55
NikK173i <160059242+NikK173i@users.noreply.github.com>
2024-05-15 20:54:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,22 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        exit(1);
+    }
+    char symbol;
+    std::string speech;
+    BST<std::string> tree;
+    while (file.get(symbol)) {
+        if (isalpha(symbol)) {
+            speech += tolower(symbol);
+        } else if (!speech.empty()) {
+            tree.insert(speech);
+            speech.clear();
+        }
+    }
+    file.close();
+    return tree;
 }
