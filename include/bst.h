254e04cf0a926d2145693576b1e96721123f07f7
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-05-18 22:21:37 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,56 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <string>
+#include <iostream>
+
+template <typename T>
+class BST {
+private:
+   
+    struct Node {
+        T data;        
+        int count;      
+        Node *left;     
+        Node *right;    
+
+      
+        Node(T val) : data(val), count(1), left(nullptr), right(nullptr) {}
+    };
+
+    Node *root; // Корень дерева
+
+    void insert(Node *&node, const T &val) {
+        if (!node) {
+            node = new Node(val);
+        } else if (val < node->data) {
+            insert(node->left, val);
+        } else if (val > node->data) {
+            insert(node->right, val);
+        } else {
+            node->count++;
+        }
+    }
+
+    void inOrder(Node *node) const {
+        if (node) {
+            inOrder(node->left);
+            std::cout << node->data << " - " << node->count << std::endl;
+            inOrder(node->right);
+        }
+    }
+
+public:
+    // Конструктор дерева
+    BST() : root(nullptr) {}
+
+    void insert(const T &val) {
+        insert(root, val);
+    }
+
+    void inOrder() const {
+        inOrder(root);
+    }
+};
+
 #endif  // INCLUDE_BST_H_
