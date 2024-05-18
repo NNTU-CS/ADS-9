fc10f0a2eb341dd09b4b3118f3111aac9e46386f
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-05-18 22:18:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,31 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+bool isLatinLetter(char ch) {
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+}
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "File error!" << std::endl;
+        return tree;
+    }
+    std::string word;
+    char ch;
+    while (file.get(ch)) {
+        if (isLatinLetter(ch)) {  
+            word += std::tolower(ch, std::locale());
+        } else if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
+        }
+    }
+
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+    return tree;
 }
