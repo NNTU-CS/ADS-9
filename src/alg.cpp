5eae83b6abf55c283f94a93a5ab9eab28864895b
DariaChileeva <150884364+DariaChileeva@users.noreply.github.com>
2024-05-19 13:05:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,26 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> bst;
+  std::ifstream file(filename);
+  std::string output;
+  while (!file.eof()) {
+    char chPerem = file.get();
+    if (chPerem >= 65 && chPerem <=90) {
+      chPerem +=32; 
+      output += chPerem;
+      continue;
+    }
+    if (chPerem >=97 && chPerem <= 122) {
+      output += chPerem;
+    } else {
+      if (!output.empty()) {
+        bst.addVal(output);
+      }
+      output.clear();
+    }
+  }
+  bst.addVal(output);
+  file.close();
+  return bst;
 }
