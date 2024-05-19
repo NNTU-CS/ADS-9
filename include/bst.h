5b5e5a73b98aa66559eb63133020dbc9f1499fc9
Илья Прокошин <117671181+IliaProkoshin@users.noreply.github.com>
2024-05-19 15:47:18 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,35 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <locale>
 
-#endif  // INCLUDE_BST_H_
+#include "bst.h"
+
+BST<std::string> makeTree(const char* filename) {
+  BST<std::string>* bst = new BST<std::string>();
+  std::ifstream file(filename);
+  int count = 0;
+  std::string boofer;
+  bool in_string = false;
+  while (!file.eof()) {
+    int ch = file.get();
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+      in_string = true;
+      if (ch >= 'A' && ch <= 'Z') {
+        ch = ch - 32;
+      }
+      boofer += ch;
+    } else if (in_string) {
+      bst->add(boofer);
+      in_string = false;
+      boofer.clear();
+    }
+    count++;
+  }
+  if (in_string) {
+    bst->add(boofer);
+  }
+  file.close();
+  return *bst;
+}
