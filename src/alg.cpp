9438c02589a31d530cd81d3a762ba8e3503989e7
artemkosterin <160392965+artemkosterin@users.noreply.github.com>
2024-05-19 19:42:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,37 +1,30 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <cctype>
+#include <locale>
+#include <cstdlib>
 #include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  BST<std::string> tree;
-  std::ifstream file(filename);
-  if (!file) {
-    std::cout << "File error!" << std::endl;
-    return tree;
-  }
-  std::string word;
-  while (file >> word) {
-    for (char& c : word) {
-      c = std::tolower(c);
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        exit(1);
     }
-    tree.add(word);
-  }
-  file.close();
 
-  return tree;
-}
+    char character;
+    std::string word;
+    BST<std::string> tree;
 
-int main() {
-  const char* filename = "src/war_peace.txt";
-  BST<std::string> tree = makeTree(filename);
-  for (int i = 0; i < 10; i++) {
-    std::string mostFrequentWord;
-    int maxCount = 0;
-    tree.searchTree(tree.root, mostFrequentWord, maxCount);
-    std::cout << mostFrequentWord << ": " << maxCount << std::endl;
-  }
-  return 0;
+    while (file.get(character)) {
+        if (isalpha(character)) {
+            word += tolower(character);
+        } else if (!word.empty()) {
+            tree.add(word);
+            word.clear();
+        }
+    }
+
+    file.close();
+    return tree;
 }
