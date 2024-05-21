b1cac956f64414f28dc21225e2c57bc7eeee06eb
artemchikkey <160394124+artemchikkey@users.noreply.github.com>
2024-05-21 17:21:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,32 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cerr << "File error!" << std::endl;
+        return tree;
+    }
+
+    std::string word;
+    char ch;
+
+    while (file.get(ch)) {
+        if (std::isalpha(ch)) {
+            word = world + std::tolower(static_cast<unsigned char>(ch));
+        } else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+    return tree;
 }
