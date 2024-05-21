263a9a21ed330b1be21eaa6490e08b1bdb1cb6ff
Daniel-Ohaio <160250909+Daniel-Ohaio@users.noreply.github.com>
2024-05-22 00:16:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
     std::ifstream file(filename);
@@ -12,19 +12,20 @@ BST<std::string> makeTree(const char* filename) {
     char sym;
     while (!file.eof()) {
         sym = file.get();
-        if (!(sym > 'Z' && sym < 'a') \
-            && (sym >= 'A' && sym <= 'z')) {
+        if (!(sym > 'Z' && sym < 'a') && \
+            (sym >= 'A' && sym <= 'z')) {
             if (sym <= 'Z') {
                 sym += 32;
             }
             speak.append(1, sym);
         } else {
             if (!speak.empty()) {
-                tree.ListAd(tree.gethost(), speak);
+                List** adr = tree.gethost();
+                tree.ListAd(adr, speak);
                 speak.clear();
             }
         }
     }
     file.close();
-  return tree;
+    return tree;
 }
