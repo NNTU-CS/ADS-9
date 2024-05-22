d318175b38d939b728b793294fc4f2b9e3a20f6b
Andre-s23 <160166204+Andre-s23@users.noreply.github.com>
2024-05-22 19:49:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,26 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+    BST<std::string> q;
+    std::string str;
+    char sim;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return q;
+    }
+    while (!file.eof())
+    {
+        sim = file.get();
+        if (('a' <= sim) && (sim <= 'z') || ('A' <= sim) && (sim <= 'Z')) {
+            str += sim;
+        } else {
+            if (str.size() != 0) {
+                q.add(str);
+                str.clear();
+            }
+        }
+    }
+    file.close();
+    return q;
 }
