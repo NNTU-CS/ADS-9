62a78dae76493a8694f12382edaa492f1d61b0ea
Frollluuu <Frolovaannet2004@yandex.ru>
2024-05-22 23:02:09 +0300

    1

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,61 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <iostream>
+#include <string>
+
+template <typename T>
+class BSTNode {
+public:
+    T data;
+    int count;
+    BSTNode<T>* left;
+    BSTNode<T>* right;
+
+    BSTNode(const T& data) : data(data), count(1), left(nullptr), right(nullptr) {}
+};
+
+template <typename T>
+class BST {
+public:
+    BSTNode<T>* root;
+
+    BST() : root(nullptr) {}
+
+    void insert(const T& data) { insert(root, data); }
+    void print() { print(root); }
+    int count(const T& data) { return count(root, data); }
+
+private:
+    void insert(BSTNode<T>*& node, const T& data) {
+        if (node == nullptr) {
+            node = new BSTNode<T>(data);
+        } else if (data < node->data) {
+            insert(node->left, data);
+        } else if (data > node->data) {
+            insert(node->right, data);
+        } else {
+            node->count++;
+        }
+    }
+    void print(BSTNode<T>* node) {
+        if (node != nullptr) {
+            print(node->left);
+            std::cout << node->data << " : " << node->count << std::endl;
+            print(node->right);
+        }
+    }
+    int count(BSTNode<T>* node, const T& data) {
+        if (node == nullptr) {
+            return 0;
+        } else if (data < node->data) {
+            return count(node->left, data);
+        } else if (data > node->data) {
+            return count(node->right, data);
+        } else {
+            return node->count;
+        }
+    }
+};
+
 #endif  // INCLUDE_BST_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,35 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+const char* filename = "war_peace.txt"
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return tree;
+    }
+
+    std::string word;
+    while (file >> word) {
+        // Преобразуем слово в нижний регистр
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+
+        // Удаляем все нелатинские символы и цифры
+        std::string filteredWord;
+        for (char c : word) {
+            if (isalpha(c)) {
+                filteredWord += c;
+            }
+        }
+
+        if (!filteredWord.empty()) {
+            tree.insert(filteredWord);
+        }
+    }
+
+    file.close();
+
+    return tree;
 }
