d4b7d5ad8f54f5a0c34f87203f03ede474f31b93
AndreyChetnev <159932012+AndreyChetnev@users.noreply.github.com>
2024-05-22 16:45:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,22 +6,24 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-   std::ifstream file(filename);
-   if (!file) {
-     std::cout << "File error!" << std::endl;
-     exit(1);
-   }
-   char a;
-   std::string word;
-   BST<std::string> tree;
-   while (file.get(a)) {
-     if (isalpha(a)) {
-         word += tolower(a);
-     } else if (!word.empty()) {
-         tree.add(word);
-         word.clear();
-     }
-   }
-   file.close();
-   return tree;
+    // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        exit(1);
+    }
+    char a;
+    std::string word;
+    BST<std::string> tree;
+    while (file.get(a)) {
+        if (isalpha(a)) {
+            word += tolower(a);
+        }
+        else if (!word.empty()) {
+            tree.add(word);
+            word.clear();
+        }
+    }
+    file.close();
+    return tree;
 }
