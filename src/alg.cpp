4ed4e29e3f6a5bc48c0831878410a8b06576ec54
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-05-22 21:09:19 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,32 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <cctype>
+#include  <algorithm>
+#include  <string>
 #include  "bst.h"
 
+std::string cleanText(std::string text) {
+    transform(text.begin(), text.end(), text.begin(), ::tolower);
+    text.erase(remove_if(text.begin(), text.end(), ::ispunct), text.end());
+    return text;
+}
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+      std::cerr << "Ошибка открытия файла: " << filename << std::endl;
+      exit(1);
+    }
+    BST<std::string> tree;
+    std::string line;
+    while (getline(file, line)) {
+        std::stringstream ss(cleanText(line));
+        std::string word;
+        while (ss >> word) {
+            tree.insert(word);
+        }
+    }
+    file.close();
+    return tree;
 }
