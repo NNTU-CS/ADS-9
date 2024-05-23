07072206b94fd32cb7403a53db22098ff45e9806
arinamalygina <160202011+arinamalygina@users.noreply.github.com>
2024-05-23 19:50:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,36 @@
 // Copyright 2021 NNTU-CS
 #include  <iostream>
-#include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include  "bst.h"
+#include <fstream>
+#include <cctype>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cerr << "Error opening file: " << filename << std::endl;
+        return tree;
+    }
+
+    std::string word;
+    while (file >> word) {
+        std::transform(word.begin(), word.end(), word.begin(), [](unsigned char c) {
+            return std::tolower(c);
+        });
+
+        for (char c : word) {
+            if (isalpha(c)) {
+                word = c;
+                tree.insert(word);
+            } else {
+                break;
+            }
+        }
+    }
+
+    file.close();
+    return tree;
 }
