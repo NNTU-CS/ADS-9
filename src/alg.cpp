2b4c40dff45baf00f63aa4b3378eb08f99009d73
Viksss16 <160354641+Viksss16@users.noreply.github.com>
2024-05-23 22:44:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,30 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
-
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST <std::string> storage;
+std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+        int count = 0;
+        std::string str;
+        bool inString = false;
+    while (!file.eof()) {
+        int ch = file.get();
+        if (ch >= 'A' && ch <= 'Z') {
+            inString = true;
+            ch = tolower(ch);
+            str += ch;
+        } else if (ch >= 'a' && ch <= 'z') {
+            inString = true;
+            str += ch;
+        } else if (inString) {
+            storage.add(str);
+            str.clear();
+        }
+    }
+    file.close();
+    return storage;
 }
