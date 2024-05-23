ab150d4a4a34eb941fb01eca8120b5d27b857da3
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-05-24 00:02:01 +0300

    bst.h

--- include/bst.h
+++ include/bst.h
@@ -5,34 +5,84 @@
 #include <algorithm>
 #include <map>
 #include <iostream>
-template <typename T>
+template<typename T>
+struct Node {
+    T data;
+    int count;
+    Node* left, * right;
+    explicit Node(T k) : data(k), count(1), left(nullptr), right(nullptr) {}
+};
+
+template<typename T>
 class BST {
  private:
-    std::map<std::string, int> data;
+   
+    Node<T>* data;
 
- public:
-    int depth() { return data.size(); }
-
-    void add(std::string element) {
-        if (data.count(element)) {
-            data[element]++;
-        } else {
-            data[element] = 1;
-        }
-    }
-
-    int search(std::string element) {
-        if (data.count(element)) {
-            return data[element];
-        } else {
+    int Height(Node<T>* p) {
+        if (p == nullptr) {
             return 0;
         }
+        return std::max(Height(p->right), Height(p->left)) + 1;
     }
 
-    void printTree() {
-        for (const auto& pair : data) {
-            std::cout << pair.first << ": " << pair.second << std::endl;
+    Node<T>* insert(Node<T>* p, T k) {
+        if (p == nullptr) {
+            return new Node<T>(k);
         }
+        else if (k < p->data) {
+            p->left = insert(p->left, k);
+        }
+        else if (k > p->data) {
+            p->right = insert(p->right, k);
+        }
+        else {
+            p->count++;
+        }
+        return p;
+    }
+
+    int find(Node<T>* p, T k) {
+        if (p == nullptr) {
+            return 0;
+        }
+        else if (k == p->data) {
+            return p->count;
+        }
+        else if (k < p->data) {
+            return findVal(p->left, k);
+        }
+        else {
+            return findVal(p->right, k);
+        }
+    }
+
+    void deleteTree(Node<T>* p) {
+        if (p == nullptr) {
+            return;
+        }
+        deleteTree(p->right);
+        deleteTree(p->left);
+        delete p;
+    }
+
+ public:
+    BST() : data(nullptr) {}
+
+    void insert(T k) {
+        data = insert(data, k);
+    }
+
+    int depth() {
+        return Height(data) - 1;
+    }
+
+    int search(T k) {
+        return find(data, k);
+    }
+
+    ~BST() {
+        deleteTree(data);
     }
 };
 #endif  // INCLUDE_BST_H_
