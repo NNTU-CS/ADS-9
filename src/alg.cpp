d549751ba05eb71a1f77a93230b8eb5f2cbb6b94
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-05-23 21:37:38 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,14 @@
 // Copyright 2021 NNTU-CS
 #include  <iostream>
 #include  <fstream>
-#include  <locale>
 #include  <cstdlib>
-#include <sstream>
-#include  <cctype>
-#include  <algorithm>
 #include  <string>
 #include  "bst.h"
 
 BST<std::string>* makeTree(const char* filename) {
     BST<std::string>* bst = new BST<std::string>();
     std::ifstream file(filename);
-    if (!file.is_open()) {
+    if (!file) {
         std::cerr << "Ошибка открытия файла: " << filename << std::endl;
         exit(1);
     }
