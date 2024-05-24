5d7a5f0f568d4ade0312bd3d816c03af37a22eec
fdlii <155849941+fdlii@users.noreply.github.com>
2024-05-24 16:49:40 +0300

    egg

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,23 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+using namespace std;
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+      ifstream fin(filename);
+    if (!fin) {
+        exit(1);
+    }
+    BST<string> t;
+    while (!fin.eof()) {
+        char ch;
+        string str;
+        fin.get(ch);
+        if (isalpha(ch))
+            str += tolower(ch);
+        else if (!str.empty())
+            t.add(str);
+    }
+    fin.close();
+    return t;
 }
