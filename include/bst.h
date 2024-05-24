e22f3cc9688716a04b86e41cd523216a69a76cef
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-05-24 09:39:23 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,43 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <iostream>
+#include <fstream>
+#include "bst.h"
+
+using namespace std;
+
+BST<std::string> makeTree(const char* filename) {
+    BST<string> bst;
+    ifstream fin(filename);
+    string str = "";
+    if (fin.is_open()) {
+        while (!fin.eof()) {
+            str += (char)fin.get();
+        }
+        fin.close();
+    } else {
+        cout << "Ошибка";
+    }
+    string currentWord = "";
+    for (int i = 0; i < str.length(); i++) {
+        if (isalpha(str[i]) ) {
+            if(isupper(str[i])) {
+                currentWord += tolower(str[i]);
+            } else {
+                currentWord += str[i];
+            }
+        } else {
+            if (currentWord != "") {
+                bst.add(currentWord);
+            }
+            currentWord = "";
+        }
+    }
+    if (currentWord != "") {
+        bst.add(currentWord);
+    }
+    return bst;
+}
 
 #endif  // INCLUDE_BST_H_
