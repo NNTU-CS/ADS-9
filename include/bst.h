b7b2e713dc5121b29c462df62658e79a3371d5da
Arina-Arahis <159996104+Arina-Arahis@users.noreply.github.com>
2024-05-24 13:42:02 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,88 +1,102 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-#pragma once
-#include <string>
-template <typename T>
-struct Node {
-    T data;
-    int count;
-    Node* left;
-    Node* right;
 
-    Node(const T& d) : data(d), count(1), left(nullptr), right(nullptr) {}
-};
+#include <algorithm>
+#include <iostream>
+#include <string>
 
 template <typename T>
 class BST {
-private:
-    Node<T>* root;
+ private:
+    struct Node {
+        T value;
+        int count;
+        Node* left, * right;
+        explicit Node(const T& val) : value(val), count(1),
+            left(nullptr), right(nullptr) {}
+    };
+    Node* root;
 
-    void insert(Node<T>*& node, const T& value) {
-        if (!node) {
-            node = new Node<T>(value);
-        } else if (value < node->data) {
-            insert(node->left, value);
-        } else if (value > node->data) {
-            insert(node->right, value);
-        } else {
+    Node* insert(Node* node, const T& value) {
+        if (!node)
+            return new Node(value);
+        if (value < node->value)
+            node->left = insert(node->left, value);
+        else if (value > node->value)
+            node->right = insert(node->right, value);
+        else
             node->count++;
-        }
+        return node;
     }
 
-    int getCount(Node<T>* node, const T& value) const {
-        if (!node) {
-            return 0;
-        }
-        if (value == node->data) {
-            return node->count;
-        } else if (value < node->data) {
-            return getCount(node->left, value);
-        } else {
-            return getCount(node->right, value);
-        }
+    int search(Node* node, const T& value) {
+        return node ? (value < node->value ? search(node->left, value) :
+            (value > node->value ? search(node->right, value)
+                : node->count)) : 0;
     }
 
-    int depth(Node<T>* node) const {
-        if (!node) {
-            return 0;
-        }
-        int leftDepth = depth(node->left);
-        int rightDepth = depth(node->right);
-        return std::max(leftDepth, rightDepth) + 1;
+    int depth(Node* node) {
+        return node ? 1 + std::max(depth(node->left),
+            depth(node->right)) : 0;
     }
 
-    int search(Node<T>* node, const T& value, int depth) const {
-        if (!node) {
-            return -1;
+    void findMostFrequent(Node* node, T& mostFrequentWord, int& maxCount) {
+        if (!node)
+            return;
+        findMostFrequent(node->right, mostFrequentWord, maxCount);
+        if (node->count > maxCount) {
+            mostFrequentWord = node->value;
+            maxCount = node->count;
         }
-        if (node->data == value) {
-            return depth;
-        }
-        int leftDepth = search(node->left, value, depth + 1);
-        if (leftDepth != -1) {
-            return leftDepth;
-        }
-        return search(node->right, value, depth + 1);
+        findMostFrequent(node->left, mostFrequentWord, maxCount);
     }
 
-public:
+    Node* remove(Node* node, const T& value) {
+        if (!node)
+            return nullptr;
+        if (value < node->value)
+            node->left = remove(node->left, value);
+        else if (value > node->value)
+            node->right = remove(node->right, value);
+        else {
+            if (!node->left) {
+                Node* temp = node->right;
+                delete node;
+                return temp;
+            } else if (!node->right) {
+                Node* temp = node->left;
+                delete node;
+                return temp;
+            }
+            node->value = findMin(node->right)->value;
+            node->right = remove(node->right, node->value);
+        }
+        return node;
+    }
+
+    Node* findMin(Node* node) {
+        while (node->left)
+            node = node->left;
+        return node;
+    }
+
+ public:
     BST() : root(nullptr) {}
 
-    void insert(const T& value) {
-        insert(root, value);
+    void add(const T& value) { root = insert(root, value); }
+
+    int search(const T& value) { return search(root, value); }
+
+    int depth() { return depth(root) - 1; }
+
+    void findMostFrequent(T& mostFrequentWord, int& maxCount) {
+        mostFrequentWord = "";
+        maxCount = 0;
+        findMostFrequent(root, mostFrequentWord, maxCount);
     }
 
-    int getCount(const T& value) const {
-        return getCount(root, value);
-    }
-
-    int depth() const {
-        return depth(root);
-    }
-
-    int search(const T& value) const {
-        return search(root, value, 0);
-    }
+    void remove(const T& value) { root = remove(root, value); }
 };
+
 #endif  // INCLUDE_BST_H_
