2501588dd816d0e980b5ec841185942346b2082f
platna <160319937+platna@users.noreply.github.com>
2024-05-24 11:18:25 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,28 +1,71 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <algorithm>
 #include <string>
+template<typename T>
+struct Node {
+    T key;
+    int count;
+    Node *left, *right;
 
-template <typename T>
+    explicit Node(T k) : key(k), count(1), left(nullptr), right(nullptr) {}
+};
+template<typename T>
 class BST {
-public:
-    struct Node {
-        T data;
-        int count;
-        Node* left;
-        Node* right;
+ private:
+    Node<T> *root;
 
-        Node(const T& data) : data(data), count(1), left(nullptr), right(nullptr) {}
-    };
-
+    int Height(Node<T> *p) {
+        if (p == nullptr)
+            return 0;
+        int hr = Height(p->right);
+        int hl = Height(p->left);
+        return std::max(hr, hl) + 1;
+    }
+    Node<T> *insert(Node<T> *p, T k) {
+        if (p == nullptr) {
+            p = new Node<T>(k);
+        } else if (p->key > k) {
+            p->left = insert(p->left, k);
+        } else if (p->key < k) {
+            p->right = insert(p->right, k);
+        } else {
+            p->count++;
+        }
+        return p;
+    }
+    int findVal(Node<T> *p, T k) {
+        if (p->key == k)
+            return p->count;
+        else if (p->key > k)
+            return findVal(p->left, k);
+        else if (p->key < k)
+            return findVal(p->right, k);
+        else
+            return 0;
+    }
+    void deleteTree(Node<T> *p) {
+        if (p == nullptr)
+            return;
+        deleteTree(p->right);
+        deleteTree(p->left);
+        delete p;
+        p = nullptr;
+    }
+ public:
     BST() : root(nullptr) {}
-    ~BST() { clear(root); }
-
-    void insert(const T& data);
-    int find(const T& data) const;
-    void clear(Node* node);
-
-private:
-    Node* root;
+    void insert(T k) {
+        root = insert(root, k);
+    }
+    int depth() {
+        return Height(root) - 1;
+    }
+    int search(T k) {
+        return findVal(root, k);
+    }
+    ~BST() {
+        deleteTree(root);
+    }
 };
 #endif  // INCLUDE_BST_H_
