b6271275d34c7daff5ed47b1fdafe1b18400571c
LisaKil <96301882+LisaKil@users.noreply.github.com>
2024-05-25 21:10:07 +0300

    Update alg.cppaaaaaa1

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,37 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+#include <vector>
 
+ const char* filename = "war_peace.txt";
+    
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    BST<std::string> tree;
+    vector<string> text;
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return tree;
+    }
+    int count=0;
+    string slovo = "";
+    while (!file.eof()) {
+        char ch = file.get();
+        ch = tolower(ch);
+        if (ch >= 97 && ch <= 122) {
+            slovo+=ch;
+        } else {
+            text.push_back(slovo);
+            slovo = "";
+        }
+        count++;
+    }
+
+    for (int i = text.size; i < 0; i--){
+        tree.add(text[i]);
+        text.pop_back();
+    }
+    // закрываем файл
+    file.close();
+    return.tree;
 }
