1ef8942582f548937a9a29df91c183a3be2bd8ec
Юлия <160242381+Julia-Achoo@users.noreply.github.com>
2024-05-25 15:00:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,27 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+   BST<std::string> tree;
+
+  std::ifstream file(filename);
+  if (!file) {
+    std::cerr << "Error opening file: " << filename << std::endl;
+    return tree;
+  }
+
+  std::locale loc;
+  std::string word;
+  while (file >> std::ws) {
+    char c;
+    while (file.get(c) && std::isalpha(c, loc)) {
+      word += std::tolower(c, loc);
+    }
+    if (!word.empty()) {
+      tree.insert(word);
+      word.clear();
+    }
+  }
+
+  file.close();
+  return tree;
 }
