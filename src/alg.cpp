ecbd838e5f33837dbc0b78ea6eb0d546b142a7a0
AnastasiaKotina <160050075+AnastasiaKotina@users.noreply.github.com>
2024-05-25 18:33:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,31 @@
 // Copyright 2021 NNTU-CS
 #include  <iostream>
 #include  <fstream>
-#include  <locale>
-#include  <cstdlib>
 #include  "bst.h"
+#include <sstream>
+#include <regex>
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::ifstream file(filename);
+
+  if (!file.is_open()) {
+      throw std::runtime_error("Ошибка открытия файла");
+  }
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  file.close();
+
+  std::string content = buffer.str();
+  std::regex wordRegex("[a-zA-Z]+");
+  std::smatch match;
+
+  while (std::regex_search(content, match, wordRegex)) {
+      std::string word = match.str();
+      std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+      tree.insert(word);
+      content = match.suffix();
+  }
+  return tree;
 }
