1c15a40629d6026382e4f592aa85fb88a22ff67a
Evgeniy <160057044+K1r1gam1@users.noreply.github.com>
2024-05-25 21:51:31 +0300

    B56.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,24 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<T> makeTree(const char* filename) {
+    BST<std::string> bst;
+    std::ifstream file(filename);
+    std::string word;
+    
+    if (file.is_open()) {
+        while (file >> word) {
+            for (char& c : word) {
+                if (c >= 'A' && c <= 'Z') {
+                    c = tolower(c);
+                }
+            }
+            bst.add(word);
+        }
+        file.close();
+    } else {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+    }
+    
+    return bst;
 }
