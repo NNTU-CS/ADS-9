f48d53d97a63827f23790719598f0cbbb7bd02a6
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-05-25 22:00:24 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,38 +1,53 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+
 template <typename T>
 class BST {
  public:
-    BST() : root(nullptr) {}
-    ~BST();
-
-    void insert(const T& data);
-    void inorderTraversal() const;
-    int size() const;
-
- private:
+    BST(){
+        root(nullptr) 
+    }
     struct Node {
         T data;
         int count;
         Node* left;
         Node* right;
-        Node(const T& data) : data(data), count(1), left(nullptr), right(nullptr) {}
+        Node(const T& data) {
+            data(data);
+            count(1);
+            left(nullptr);
+            right(nullptr);
+        }
     };
+    void insert(const T& data);
+    void inorderTraversal() const;
+    int size() const;
+    ~BST();
+
+ private:
     Node* root;
-    void insert(Node*& node, const T& data);
-    void inorderTraversal(Node* node) const;
-    int size(Node* node) const;
+    void insertN(Node*& node, const T& data);
+    void inorderTraversalN(Node* node) const;
+    int sizeN(Node* node) const;
 };
+
+BST<std::string> makeTree(const char* filename);
+
 template <typename T>
 BST<T>::~BST() {
+    if (root) {
+        delTree(root);
+    }
 }
+
 template <typename T>
 void BST<T>::insert(const T& data) {
     insert(root, data);
 }
+
 template <typename T>
-void BST<T>::insert(Node*& node, const T& data) {
+void BST<T>::insertN(Node*& node, const T& data) {
     if (node == nullptr) {
         node = new Node(data);
         return;
@@ -45,24 +60,28 @@ void BST<T>::insert(Node*& node, const T& data) {
         node->count++;
     }
 }
+
 template <typename T>
 void BST<T>::inorderTraversal() const {
     inorderTraversal(root);
 }
+
 template <typename T>
-void BST<T>::inorderTraversal(Node* node) const {
+void BST<T>::inorderTraversalN(Node* node) const {
     if (node != nullptr) {
         inorderTraversal(node->left);
         std::cout << node->data << " : " << node->count << std::endl;
         inorderTraversal(node->right);
     }
 }
+
 template <typename T>
 int BST<T>::size() const {
     return size(root);
 }
+
 template <typename T>
-int BST<T>::size(Node* node) const {
+int BST<T>::sizeN(Node* node) const {
     if (node == nullptr) {
         return 0;
     }
