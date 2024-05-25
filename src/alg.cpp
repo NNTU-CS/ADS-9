600458535110d9445e55d8d2dab20b4240d99e07
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-05-25 21:48:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,37 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+std::string toLower(const std::string& str) {
+    std::string lowerStr = str;
+    for (char& c : lowerStr) {
+        c = tolower(c);
+    }
+    return lowerStr;
+}
+
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cout << "File error!" << std::endl;
+        return tree;
+    }
+    std::string word;
+    char ch;
+    while (file.get(ch)) {
+        if (isalpha(ch)) {
+            word += ch;
+        }
+        else {
+            if (!word.empty()) {
+                tree.insert(toLower(word));
+                word.clear();
+            }
+        }
+    }
+    if (!word.empty()) {
+        tree.insert(toLower(word));
+    }
+    file.close();
+    return tree;
 }
