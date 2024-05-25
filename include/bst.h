e8748cbc6de1a753372b921f8bcd65af3bc9ce1a
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-05-25 23:03:54 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,10 +4,7 @@
 
 template <typename T>
 class BST {
-public:
-    BST() {
-        root = nullptr;
-    }
+private:
     struct Node {
         T data;
         int count;
@@ -20,26 +17,6 @@ public:
             right = nullptr;
         }
     };
-    void insert(const T& data) {
-        root = insertN(root, data);
-    }
-    void inorderTraversal() const;
-    int search(const T& value) {
-        Node* curr = searchT(root, value);
-        if (curr == nullptr) {
-            return 0;
-        }
-        else {
-            return curr->countt;
-        }
-    }
-    ~BST() {
-        if (root) {
-            delTree(root);
-        }
-    }
-
-private:
     Node* root;
     int inorderTraversalN(Node* root) {
         if (root == nullptr) {
@@ -75,6 +52,28 @@ private:
 
     void BST<T>::inorderTraversal() const {
         return inorderTraversalN(root) - 1;
+    }
+public:
+    BST() {
+        root = nullptr;
+    }
+    void insert(const T& data) {
+        root = insertN(root, data);
+    }
+    void inorderTraversal() const;
+    int search(const T& value) {
+        Node* curr = searchT(root, value);
+        if (curr == nullptr) {
+            return 0;
+        }
+        else {
+            return curr->countt;
+        }
+    }
+    ~BST() {
+        if (root) {
+            delTree(root);
+        }
     }
 };
 #endif  // INCLUDE_BST_H_
