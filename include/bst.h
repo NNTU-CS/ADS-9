5b4cf85940f63ddf1e3ebd7869a9584699f4b6fd
AndreyMatuhin99 <tronder.yt@gmail.com>
2024-05-25 13:59:42 +0300

    Задание 9

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,67 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
+#ifndef BST_H
+#define BST_H
+#include <string>
+#include <iostream>
 
-#endif  // INCLUDE_BST_H_
+template<typename T>
+struct TreeNode {
+    T key;
+    int frequency;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(const T& k) : key(k), frequency(1), left(nullptr), right(nullptr) {}
+};
+
+template<typename T>
+class BST {
+public:
+    BST() : root(nullptr) {}
+
+    void insert(const T& key);
+    void inOrderPrint() const;
+
+private:
+    TreeNode<T>* root;
+
+    TreeNode<T>* insert(TreeNode<T>* node, const T& key);
+    void inOrderPrint(TreeNode<T>* node) const;
+};
+
+template<typename T>
+void BST<T>::insert(const T& key) {
+    root = insert(root, key);
+}
+
+template<typename T>
+TreeNode<T>* BST<T>::insert(TreeNode<T>* node, const T& key) {
+    if (node == nullptr) {
+        return new TreeNode<T>(key);
+    }
+    if (key == node->key) {
+        node->frequency++;
+    } else if (key < node->key) {
+        node->left = insert(node->left, key);
+    } else {
+        node->right = insert(node->right, key);
+    }
+    return node;
+}
+
+template<typename T>
+void BST<T>::inOrderPrint() const {
+    inOrderPrint(root);
+}
+
+template<typename T>
+void BST<T>::inOrderPrint(TreeNode<T>* node) const {
+    if (node != nullptr) {
+        inOrderPrint(node->left);
+        std::cout << node->key << ": " << node->frequency << std::endl;
+        inOrderPrint(node->right);
+    }
+}
+
+#endif // BST_H
+
--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,34 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+#include <fstream>
+#include <cctype>
+#include <string>
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cerr << "File error!" << std::endl;
+        return tree;
+    }
+
+    std::string word;
+    char ch;
+
+    while (file.get(ch)) {
+        if (std::isalpha(ch)) {
+            word += std::tolower(ch);
+        } else if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
+        }
+    }
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+    return tree;
 }
