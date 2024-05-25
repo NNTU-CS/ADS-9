9d216c537169021184d833618b04e59cfb7e4a13
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-05-25 23:58:40 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,7 +4,7 @@
 #include <algorithm>
 template <typename T>
 class BST {
-private:
+ private:
     struct Node {
         T data;
         int count;
@@ -43,7 +43,8 @@ private:
         }
         return searchThree(root->right, data);
     }
-public:
+
+ public:
     BST() : root(nullptr) {}
     void insert(const T& data) {
         root = addN(root, data);
@@ -55,8 +56,7 @@ public:
         Node* curr = searchThree(root, data);
         if (curr == nullptr) {
             return 0;
-        }
-        else {
+        } else {
             return curr->count;
         }
     }
