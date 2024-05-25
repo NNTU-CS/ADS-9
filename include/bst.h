c5a9b13ea02ee34da41bf39eb944de3ba20016da
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-05-25 22:52:33 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,8 +4,8 @@
 
 template <typename T>
 class BST {
- public:
-    BST(){
+public:
+    BST() {
         root = nullptr;
     }
     struct Node {
@@ -20,74 +20,64 @@ class BST {
             right = nullptr;
         }
     };
-    void insert(const T& data);
+    void insert(const T& data) {
+        root = insertN(root, data);
+    }
     void inorderTraversal() const;
-    int size() const;
+    int search(const T& value) {
+        Node* curr = searchT(root, value);
+        if (curr == nullptr) {
+            return 0;
+        }
+        else {
+            return curr->cnt;
+        }
+    }
     ~BST();
 
- private:
+private:
     Node* root;
-    void insertN(Node*& node, const T& data);
-    void inorderTraversalN(Node* node) const;
-    int sizeN(Node* node) const;
+    int inorderTraversalN(Node* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        return std::max(inorderTraversalN(root->left), inorderTraversalN(root->right)) + 1;
+    }
+    Node* addN(Node* root, const T& value) {
+        if (root == nullptr) {
+            root = new Node;
+            root->value = value;
+            root->cnt = 1;
+            root->left = root->right = nullptr;
+        } else if (root->value > value) {
+            root->left = addNode(root->left, value);
+        } else if (root->value < value) {
+            root->right = addNode(root->right, value);
+        } else {
+            root->cnt += 1;
+        }
+        return root;
+    }
+    template <typename T>
+    BST<T>::~BST() {
+        if (root) {
+            delTree(root);
+        }
+    }
+
+    Node* searchT(Node* root, const T& value) {
+        if (root == nullptr || root->value == value) {
+            return root;
+        }
+        if (value < root->value) {
+            return searchT(root->left, value);
+        }
+        return searchT(root->right, value);
+    }
+
+    template <typename T>
+    void BST<T>::inorderTraversal() const {
+        return inorderTraversal(root) - 1;
+    }
 };
-
-BST<std::string> makeTree(const char* filename);
-
-template <typename T>
-BST<T>::~BST() {
-    if (root) {
-        delTree(root);
-    }
-}
-
-template <typename T>
-void BST<T>::insert(const T& data) {
-    root = insertN(root, data);
-}
-
-template <typename T>
-void BST<T>::insertN(Node*& node, const T& data) {
-    if (root == nullptr) {
-        root = new Node;
-        root->value = value;
-        root->count = 1;
-        root->left = root->right = nullptr;
-    } else if (root->value < value) {
-        root->right = addNode(root->right, value);
-    }
-    else if (root->value > value) {
-        root->left = addNode(root->left, value);
-    } else {
-        root->count++;
-    }
-    return root;
-}
-
-template <typename T>
-void BST<T>::inorderTraversal() const {
-    return inorderTraversal(root) - 1;
-}
-
-template <typename T>
-void BST<T>::inorderTraversalN(Node* node) const {
-    if (node != nullptr) {
-        inorderTraversal(node->left);
-        std::cout << node->data << " : " << node->count << std::endl;
-        inorderTraversal(node->right);
-    }
-}
-
-template <typename T>
-int BST<T>::size() const {
-    return size(root);
-}
-
-template <typename T>
-int BST<T>::sizeN(Node* node) const {
-    if (node == nullptr) {
-        return 0;
-    }
-    return 1 + size(node->left) + size(node->right);
-}
 #endif  // INCLUDE_BST_H_
