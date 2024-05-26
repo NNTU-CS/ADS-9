d6bc821cb6abc017dda78ddc7ca3ce34351e0482
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-05-26 22:05:48 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,31 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_BST_H_
-#define INCLUDE_BST_H_
-
-#endif  // INCLUDE_BST_H_
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "bst.h"
+BST<std::string> makeTree(const char* filename) {
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return BST<std::string>();
+    }
+    BST<std::string> tree;
+    std::string word;
+    char c;
+    while (file.get(c)) {
+        if (isalpha(c)) {
+            word += tolower(c);
+        } else {
+          if (!word.empty()) {
+            tree.insert(word);
+            word.clear();
+          }
+        }
+    }
+  if (!word.empty()) {
+    tree.insert(word);
+  }
+    file.close();
+    return tree;
+}
