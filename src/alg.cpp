16bfa58474006da004580aa00a47aeb69b9bab40
Jalokan <150829310+Jalokan@users.noreply.github.com>
2024-05-26 16:53:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,25 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream file(filename);
+    std::string line;
+    std::string text;
+    std::string push;
+    while (std::getline(line, file)) {
+        text = text + line + " ";
+    }
+    for (auto c : text) {
+        if (isalpha(c)) {
+            c = tolower(c);
+            push += c;
+        }
+        else {
+            tree.addElement(push);
+            push = "";
+        }
+    }
+
+    file.close();
+    return tree;
 }
