a734e7a809b3ae26a38e8bad07bdf16d935c841d
VadimKochnev <donskoy_kek@mail.ru>
2024-05-26 19:09:01 +0300

    b259

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,53 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#include <string>
+#include <algorithm>
+
+template<typename T>
+class Node {
+ public:
+    T key;
+    Node *left, *right;
+
+    explicit Node(T k): key(k), left(nullptr), right(nullptr) {}
+};
+
+template<typename T>
+class BST {
+ private:
+    Node<T> *root;
+
+    void insert(Node<T> *node, const T &key) {
+        if (node->key == key) {
+            return;
+        } else if (node->key > key) {
+            if (node->left == nullptr) {
+                node->left = new Node<T>(key);
+                return;
+            }
+
+            insert(node->left, key);
+        } else {
+            if (node->left == nullptr) {
+                node->left = new Node<T>(key);
+                return;
+            }
+
+            insert(node->right, key);
+        }
+    }
+ public:
+    BST(): root(nullptr) {}
+
+    void insert(const T &key) {
+        if (root == nullptr) {
+            root = new Node<T>(key);
+            return;
+        }
+
+        insert(root, key);
+    }
+};
 
 #endif  // INCLUDE_BST_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,20 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    std::string word;
+    BST<std::string> tree;
+
+    while (!file.eof()) {
+        char c = file.get();
+
+        if (std::isalpha(c)) {
+            word += c;
+        } else if (word.length() != 0) {
+            tree.insert(word);
+            word = "";
+        }
+    }
+    
+    return tree;
 }
