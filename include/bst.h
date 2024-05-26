ee8c2479ee6095d5f4164d7e878db068f2cd6c5b
yegor52 <yegorbeat2005@gmail.com>
2024-05-26 04:21:08 +0300

    АДС9

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,72 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+
+template<typename T>
+class BST {
+public:
+    BST() : root(nullptr) {}
+
+    void AddValue(const T& value) {
+        root = addNode(root, value);
+    }
+
+    int search(const T& value) {
+        return searchNode(root, value);
+    }
+
+    int depth() {
+        return getDepth(root) - 1;
+    }
+
+private:
+    struct BSTNode {
+        BSTNode() : count(0), left(nullptr), right(nullptr) {}
+        T value;
+        int count;
+        BSTNode* left;
+        BSTNode* right;
+    };
+
+    BSTNode* root;
+
+    BSTNode* addNode(BSTNode* root, const T& value) {
+        if (root == nullptr) {
+            root = new BSTNode;
+            root->value = value;
+            root->left = nullptr;
+            root->right = nullptr;
+            root->count = 1;
+        } else if (root->value > value) {
+            root->right = addNode(root->right, value);
+        } else if (root->value < value) {
+            root->left = addNode(root->left, value);
+        } else {
+            root->count++;
+        }
+        return root;
+    }
+
+    int searchNode(BSTNode* root, const T& value) {
+        if (root == nullptr) {
+            return 0;
+        } else if (root->value < value) {
+            return searchNode(root->left, value);
+        } else if (root->value > value) {
+            return searchNode(root->right, value);
+        } else if (root->value == value) {
+            return root->count;
+        }
+        return 0;
+    }
+
+    int getDepth(BSTNode* root) {
+        if (!root) {
+            return 0;
+        }
+        int rightTree = getDepth(root->right);
+        int leftTree = getDepth(root->left);
+        return std::max(rightTree, leftTree) + 1;
+    }
+};
 
 #endif  // INCLUDE_BST_H_
