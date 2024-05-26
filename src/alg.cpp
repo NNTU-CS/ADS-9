e664bf333509142a892aa05b46b23c164b17b7f3
UlianaKustova <91365332+UlianaKustova@users.noreply.github.com>
2024-05-26 20:48:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,10 @@
 // Copyright 2021 NNTU-CS
-#include  "bst.h"
 #include  <iostream>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <string>
+#include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
     BST<std::string> tree;
