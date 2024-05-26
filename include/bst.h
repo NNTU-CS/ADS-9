405e6cc95d95fff3ffe58ba330fab007dac7d78c
Kotyara21 <73665937+Kotyara21@users.noreply.github.com>
2024-05-26 22:56:05 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,30 +1,83 @@
 // Copyright 2021 NNTU-CS
+#ifndef INCLUDE_BST_H_
+#define INCLUDE_BST_H_
+
 #include <iostream>
-#include <fstream>
-#include <locale>
-#include <cstdlib>
-#include "bst.h"
-
-BST<std::string> makeTree(const char* filename) {
-    std::ifstream file(filename);
-    if (!file) {
-        std::cout << "File error!" << std::endl;
-        exit(1);
+#include <string>
+template <typename T>
+class BST {
+public:
+    struct Node {
+        T val;
+        int cnt;
+        Node* left;
+        Node* right;
+    };
+private:
+    Node* root;
+    Node* addNode(Node*, T);
+    int depthTree(Node*);
+    int searchNode(Node*, T);
+public:
+    BST();
+    void add(T);
+    int depth();
+    int search(T);
+};
+template <typename T>
+BST <T>::BST() : root(nullptr) {}
+template <typename T>
+typename BST <T>::Node* BST <T>::addNode(Node* root, T val) {
+    if (root == nullptr) {
+        root = new Node;
+        root->val = val;
+        root->cnt = 1;
+        root->left = root->right = nullptr;
+    } else if (root->val > val) {
+        root->left = addNode(root->left, val);
+    } else if (root->val < val) {
+        root->right = addNode(root->right, val);
+    } else {
+        root->cnt++;
     }
-
-    char с;
-    std::string word;
-    BST<std::string> tree;
-
-    while (file.get(с)) {
-        if (isalpha(с)) {
-            word += tolower(с);
-        } else if (!word.empty()) {
-            tree.add(word);
-            word.clear();
+    return root;
+}
+template <typename T>
+void BST <T>::add(T val) {
+    root = addNode(root, val);
+}
+template <typename T>
+int BST<T>::depthTree(Node* root) {
+    if (root == nullptr) {
+        return 0;
+    } else {
+        int leftDepth = depthTree(root->left);
+        int rightDepth = depthTree(root->right);
+        if (leftDepth > rightDepth) {
+            return leftDepth + 1;
+        } else {
+            return rightDepth + 1;
         }
     }
-
-    file.close();
-    return tree;
 }
+template <typename T>
+int BST<T>::depth() {
+    return depthTree(root) - 1;
+}
+BST<std::string> makeTree(const char* filename);
+template <typename T>
+int BST<T>::searchNode(Node* root, T val) {
+    if (root == nullptr)
+        return 0;
+    else if (root->val == val)
+        return root->cnt;
+    else if (root->val > val)
+        return searchNode(root->left, val);
+    else
+        return searchNode(root->right, val);
+}
+template <typename T>
+int BST<T>::search(T v) {
+    return searchNode(root, v);
+}
+#endif  // INCLUDE_BST_H_
