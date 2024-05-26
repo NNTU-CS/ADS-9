26bafff27161c3d1f86bc5f19e22e3531cd83044
yegor52 <yegorbeat2005@gmail.com>
2024-05-26 04:08:00 +0300

    АДС9

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,28 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream inputFile(filename);
+    if (!inputFile.is_open()) {
+        throw std::string("File not found");
+    }
+
+    std::string currentWord = "";
+    BST<std::string> binarySearchTree;
+
+    while (inputFile >> currentWord) {
+        std::string word = "";
+        for (char symbol : currentWord) {
+            if ((symbol == 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z')) {
+                word += tolower(symbol);
+            }
+        }
+        binarySearchTree.AddValue(word);
+    }
+
+    return binarySearchTree;
 }
