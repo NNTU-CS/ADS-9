225ba4ddcfc2d431cb7243708a225458da77e154
IanaVatagina <150849744+IanaVatagina@users.noreply.github.com>
2024-05-26 16:43:07 +0300

    Update alg.cpp_1

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,22 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file)
+    std::cout << "File error!" << std::endl;
+  std::ifstream file(filename);
+  BST<std::string> finalTree;
+  std::string currentWordInFile;
+  while (!file.eof()) {
+    char currentSymbol = file.get();
+    if (currentSymbol >= 'A' && currentSymbol <= 'Z') {
+      currentWordInFile.append(1, currentSymbol);
+    } else if (currentWordInFile.size() != 0) {
+      finalTree.addValue(currentWordInFile);
+      currentWordInFile.clear();
+    }
+  }
+  while (currentWordInFile.size() != 0)
+    finalTree.addValue(currentWordInFile);
+  return finalTree;
 }
