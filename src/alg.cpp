18c2055f7941beb212fd5238e76b0cf62c2cb1c4
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-05-26 18:39:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,32 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> tree;
+    std::ifstream file(filename);
+
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return tree;
+    }
+
+    std::string word;
+    char ch;
+
+    while (file.get(ch)) {
+        if (isalpha(ch)) {
+            word += tolower(ch);
+        } else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+
+    if (!word.empty()) {
+        tree.insert(word);
+    }
+
+    file.close();
+    return tree;
 }
