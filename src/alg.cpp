818e1e581e1e1db7139e094a33927d3c80a0265b
Ksenia-Kvashnina <151324796+Ksenia-Kvashnina@users.noreply.github.com>
2024-05-26 18:09:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,25 +6,25 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-BST<std::string> tree;
-std::string Word;
-std::ifstream File(filename);
-if (!File.is_open()) {
-  throw std::string("Error");
-}
-while (File) {
-  char Letter = File.get();
-  if (isalpha(Letter)) {
-    Letter = tolower(Letter);
-    Word += Letter;
-  } else if (!Word.empty()) {
-    tree.add(Word);
-    Word.clear();
+  BST<std::string> tree;
+  std::string Word;
+  std::ifstream File(filename);
+  if (!File.is_open()) {
+    throw std::string("Error");
   }
-}
-if (!Word.empty()) {
-  tree.add(Word);
-}
-File.close();
-return tree;
+  while (File) {
+    char Letter = File.get();
+    if (isalpha(Letter)) {
+      Letter = tolower(Letter);
+      Word += Letter;
+    } else if (!Word.empty()) {
+      tree.add(Word);
+      Word.clear();
+    }
+  }
+  if (!Word.empty()) {
+    tree.add(Word);
+  }
+  File.close();
+  return tree;
 }
