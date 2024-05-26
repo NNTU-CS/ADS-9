47cab4d50e96c8d0d4da4632ba9bb672a61cb1c2
NazarShaa <160277049+NazarShaa@users.noreply.github.com>
2024-05-26 19:11:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,19 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> BSTtree;
+    std::ifstream file(filename);
+    std::string line;
+    while (!file.eof()) {
+        char sym = file.get();
+        if (sym >= 'A' && sym <= 'Z')
+            sym = sym + ('a' - 'A');
+        if (sym >= 'a' && sym <= 'z') {
+            line += sym;
+        } else {
+            BSTtree.add(line);
+        line.clear();
+        }
+    }
+    return BSTtree;
 }
