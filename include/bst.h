30feecef0e621f391d4d68b015e40db0ac34faab
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-05-26 19:26:16 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,60 +2,61 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 #include <algorithm>
-template < typename T >
-class BST
-{
+template <typename T>
+class BST {
 public:
-	struct Node
-	{
-		T value;
-		int count;
-		Node* left;
-		Node* right;
-	};
+    struct Node {
+        T value;
+        int count;
+        Node* left;
+        Node* right;
+    };
+
 private:
-	Node* root;
-	Node* addNode(Node*, T);
-	int depthTree(Node*) const;
-	int searchTree(Node*, const T&) const;
+    Node* root;
+    Node* addNode(Node*, T);
+    int depthTree(Node*) const;
+    int searchTree(Node*, const T&) const;
 
 public:
-	BST();
-	~BST();
-	void add(T);
-	int depth() const;
-	int search(const T&) const;
+    BST();
+    ~BST();
+    void add(T);
+    int depth() const;
+    int search(const T&) const;
 };
-template < typename T >
-BST <T >::BST() : root(nullptr) {}
-template < typename T >
-BST <T >::~BST()
-{
-	if (root)
-		delTree(root);
+
+template <typename T>
+BST<T>::BST() : root(nullptr) {}
+
+template <typename T>
+BST<T>::~BST() {
+    if (root)
+        delTree(root);
 }
-template < typename T >
-typename BST <T >::Node* BST <T >::addNode(Node* root, T value) {
-	if (root == nullptr) {
-		root = new Node;
-		root− > value = value;
-		root− > count = 1;
-		root− > left = root− > right = nullptr;
-	}
-	else if (root− > value > value) {
-		root− > left = addNode(root− > left, value);
-	}
-	else if (root− > value < value) {
-		root− > right = addNode(root− > right, value);
-	}
-	else
-		root− > count++;
-	return root;
+
+template <typename T>
+typename BST<T>::Node* BST<T>::addNode(Node* root, T value) {
+    if (root == nullptr) {
+        root = new Node;
+        root->value = value;
+        root->count = 1;
+        root->left = root->right = nullptr;
+    } else if (root->value > value) {
+        root->left = addNode(root->left, value);
+    } else if (root->value < value) {
+        root->right = addNode(root->right, value);
+    } else {
+        root->count++;
+    }
+    return root;
 }
-template < typename T >
-void BST <T >::add(T value) {
-	root = addNode(root, value);
+
+template <typename T>
+void BST<T>::add(T value) {
+    root = addNode(root, value);
 }
+
 template <typename T>
 int BST<T>::depthTree(Node* node) const {
     if (node == nullptr) {
@@ -76,10 +77,10 @@ int BST<T>::searchTree(Node* node, const T& value) const {
     if (node == nullptr) {
         return 0;
     }
-    if (node->val == value) {
-        return node->cnt;
+    if (node->value == value) {
+        return node->count;
     }
-    if (node->val < value) {
+    if (node->value < value) {
         return searchTree(node->right, value);
     }
     return searchTree(node->left, value);
@@ -89,4 +90,5 @@ template <typename T>
 int BST<T>::search(const T& value) const {
     return searchTree(root, value);
 }
+
 #endif  // INCLUDE_BST_H_
