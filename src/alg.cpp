16352148ede01e78e5895c499e56bd8c0db41e60
PivovarovAndrei <160382197+PivovarovAndrei@users.noreply.github.com>
2024-05-26 13:15:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,9 @@
 // Copyright 2021 NNTU-CS
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include  "bst.h"
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -7,7 +12,7 @@
 #include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-// поместите сюда свой код
+  // поместите сюда свой код
   BST<std::string>* bst = new BST<std::string>();
   std::ifstream file(filename);
   int count = 0;
@@ -21,15 +26,17 @@ BST<std::string> makeTree(const char* filename) {
         ch = ch - 32;
       }
       boofer += ch;
-    } else if (in_string)
+    } else if (in_string) {
       bst->add(boofer);
       in_string = false;
-      boofer.clear();}
-      count++;
+      boofer.clear();
+    }
+    count++;
+  }
+  if (in_string) {
+    bst->add(boofer);
+  }
+  file.close();
+  return *bst;
 }
-if (in_string) {
-  bst->add(boofer);
-}
-file.close();
-return *bst;
-}
+Footer
