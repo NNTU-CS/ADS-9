6b6070e7dc5fde167fcfc6b73822c36773be8823
kghstgrl <160274071+kghstgrl@users.noreply.github.com>
2024-05-27 01:06:01 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -7,10 +7,6 @@
 template <typename T>
 class BST {
  public:
-    BST();
-    int depth();
-    void append(T);
-    int search(T);
     struct Tree {
         Tree* right;
         Tree* left;
@@ -22,6 +18,11 @@ class BST {
     Tree* newTree(Tree*, T);
     Tree* treeRoot;
     int findTree(Tree*, T);
+ public:
+    BST();
+    int depth();
+    void add(T);
+    int search(T);
 };
 template <typename T>
 BST <T>::BST() : treeRoot(nullptr) {}
@@ -32,31 +33,31 @@ typename BST <T>::Tree* BST <T>::newTree(Tree* treeRoot, T info) {
         (*treeRoot).info = info;
         (*treeRoot).с = 1;
         (*treeRoot).left = (*treeRoot).right = nullptr;
-    } else if ((*treeRoot).info < info) {
-        (*treeRoot).right = newTree((*treeRoot).right, info);
     } else if ((*treeRoot).info > info) {
         (*treeRoot).left = newTree((*treeRoot).left, info);
+    } else if ((*treeRoot).info < info) {
+        (*treeRoot).right = newTree((*treeRoot).right, info);
     } else {
         (*treeRoot).с++;
     }
     return treeRoot;
 }
 template <typename T>
-void BST <T>::append(T info) {
+void BST <T>::add(T info) {
     treeRoot = newTree(treeRoot, info);
 }
 template <typename T>
 int BST<T>::getDepth(Tree* treeRoot) {
-    if (treeRoot == nullptr) {
-        return 0;
-    } else {
+    if (treeRoot != nullptr) {
         int depthLeft = getDepth((*treeRoot).left);
         int depthRight = getDepth((*treeRoot).right);
-        if (depthLeft > depthRight) {
-            return depthLeft + 1;
-        } else {
+        if (depthLeft < depthRight) {
             return depthRight + 1;
+        } else {
+            return depthLeft + 1;
         }
+    } else {
+        return 0;
     }
 }
 template <typename T>
@@ -65,15 +66,15 @@ int BST<T>::depth() {
 }
 BST<std::string> makeTree(const char* filename);
 template <typename T>
-int BST<T>::findTree(Tree* treeRoot, T value) {
+int BST<T>::findTree(Tree* treeRoot, T vval) {
     if (treeRoot == nullptr) {
         return 0;
-    } else if ((*treeRoot).info > value) {
-        return findTree((*treeRoot).left, value);
-    } else if ((*treeRoot).info == value) {
+    } else if ((*treeRoot).info == vval) {
         return (*treeRoot).с;
+    } else if ((*treeRoot).info > vval) {
+        return findTree((*treeRoot).left, vval);
     } else {
-        return findTree((*treeRoot).right, value);
+        return findTree((*treeRoot).right, vval);
     }
 }
 template <typename T>
@@ -81,3 +82,4 @@ int BST<T>::search(T f) {
     return findTree(treeRoot, f);
 }
 #endif  // INCLUDE_BST_H_
+
