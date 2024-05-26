7d42252f5cf821c64027d30a9867f5ce1fe7d9fd
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-05-26 20:20:58 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -4,21 +4,19 @@
 #include <algorithm>
 template <typename T>
 class BST {
-public:
+ public:
     struct Node {
         T value;
         int count;
         Node* left;
         Node* right;
     };
-
-private:
+ private:
     Node* root;
     Node* addNode(Node*, T);
     int depthTree(Node*) const;
     int searchTree(Node*, const T&) const;
-
-public:
+ public:
     BST(): root(nullptr) {}
     void add(T);
     int depth() const;
