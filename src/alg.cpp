6585aeb091bd1cd5bce99990d7473db5e88c4d0d
olesiatuliapkina <151723481+olesiatuliapkina@users.noreply.github.com>
2024-05-26 17:26:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,39 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include  <vector>
+
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string> makeTree(const char *filename) {
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Error!" << std::endl;
+    }
+    BST<std::string> tree;
+    char ch;
+    char *sl = new char;
+    int counter = 0;
+    std::vector<std::string> txt;
+    while (!file.eof()) {    
+        file.get(ch);
+        if ((ch > 64 && ch < 91) || (ch > 96 && ch < 123)) {
+            sl[counter] = tolower(ch);
+            counter++
+        } else {
+            for (int i = 0; i < counter; i++) {
+                stroka += sl[i];
+            }
+            if (stroka != "") {
+                txt.push_back(stroka);
+            }
+            stroka = "";
+        }
+    }
+    while (!txt.empty()) {
+        tree.add(txt.back());
+        txt.pop_back();  
+    }
+    file.close();
+    return tree;
 }
