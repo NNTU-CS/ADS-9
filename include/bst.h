ba5cf61d37a239cec757f04f49a923f4556f4ae4
shorokhovkir <160240169+shorokhovkir@users.noreply.github.com>
2024-05-26 22:23:55 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,93 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+template <typename T>
+class BST {
+public:
+  struct Node {
+    T value;
+    int count;
+    Node* left;
+    Node* right;
+  };
+
+private:
+  Node* root;
+  Node* addNode(Node* root, T value);
+  int depthTree(Node* root);
+  int searchNode(Node* root, T value);
+
+public:
+  BST();
+  void add(T value);
+  int depth();
+  int search(T value);
+};
+
+template <typename T>
+BST<T>::BST() : root(nullptr) {}
+
+template <typename T>
+typename BST<T>::Node* BST<T>::addNode(Node* root, T value) {
+  if (root == nullptr) {
+    root = new Node;
+    root->value = value;
+    root->count = 1;
+    root->left = root->right = nullptr;
+  }
+  else if (root->value > value) {
+    root->left = addNode(root->left, value);
+  }
+  else if (root->value < value) {
+    root->right = addNode(root->right, value);
+  }
+  else {
+    root->count++;
+  }
+  return root;
+}
+
+template <typename T>
+void BST<T>::add(T value) {
+  root = addNode(root, value);
+}
+
+template <typename T>
+int BST<T>::depthTree(Node* root) {
+  if (root == nullptr) {
+    return 0;
+  }
+  else {
+    int leftDepth = depthTree(root->left);
+    int rightDepth = depthTree(root->right);
+    return std::max(leftDepth, rightDepth) + 1;
+  }
+}
+
+template <typename T>
+int BST<T>::depth() {
+  return depthTree(root) - 1;
+}
+
+template <typename T>
+int BST<T>::searchNode(Node* root, T value) {
+  if (root == nullptr)
+    return 0;
+  else if (root->value == value)
+    return root->count;
+  else if (root->value > value)
+    return searchNode(root->left, value);
+  else
+    return searchNode(root->right, value);
+}
+
+template <typename T>
+int BST<T>::search(T value) {
+  return searchNode(root, value);
+}
+
 #endif  // INCLUDE_BST_H_
