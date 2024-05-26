a3dd058c68b5be48a8221b4a0b1933337774a358
nrtcru <94432719+nrtcru@users.noreply.github.com>
2024-05-26 18:11:51 +0300

    b54 update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,28 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> BSTtree;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return;
+    }
+    std::string line;
+    while (!file.eof()) {
+        char sym = file.get();
+        if (sym >= 'A' && sym <= 'Z')
+            sym = sym + ('a' - 'A');
+        if (sym >= 'a' && sym <= 'z') {
+            line += sym;
+        } else {
+            BSTtree.add(line);
+        line.clear();
+        }
+    }
+    return BSTtree;
 }
