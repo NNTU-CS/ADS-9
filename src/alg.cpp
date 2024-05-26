8e5f91a6185cf3e665be3e6dd6da36de401daac4
JulietM <150517302+JuliaMuran@users.noreply.github.com>
2024-05-26 18:40:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,7 +12,7 @@ BST<std::string> makeTree(const char* filename) {
     string str = "";
     if (fin.is_open()) {
         while (!fin.eof()) {
-            str += fin.get();
+            str += (char)fin.get();
         }
         fin.close();
     } else {
