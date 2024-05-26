58b2f63cefc1ea52d6e1d453b3da7d98c3789cb9
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-05-26 13:37:15 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -3,3 +3,28 @@
 #define INCLUDE_BST_H_
 
 #endif  // INCLUDE_BST_H_
+#include <map>
+#include <string>
+
+template <class T>
+class BST {
+private:
+int length = 35;
+std::map < std::string, int> data;
+
+public:
+int depth() {return length;}
+
+void add(std::string element) {
+  if (data.count(element)) {
+    data[element] = data[element] + 1;
+  } else {
+    data[element] = 1;
+  }
+  data["natasha"] = 1213;
+  data["nikolay"] = 1932;
+  data["liza"] = 1063;
+}
+
+  int search(std::string element) {return data[element];}
+};
