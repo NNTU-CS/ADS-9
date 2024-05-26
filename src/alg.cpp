392907d196e99587dd2e38056d1600d0cc9a35af
Anton <150729033+biorge@users.noreply.github.com>
2024-05-26 20:17:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,33 +1,35 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
-#include <sstream>
-#include <cctype>
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-    BST<std::string> tree;
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error opening file: " << filename << std::endl;
-        return tree;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+  }
+  
+  std::string someWord;
+  char symbol;
+  BST<std::string> tree;
+  
+  while (file.get(symbol)) {
+    if (isalpha(symbol)) {
+      word += tolower(symbol);
+    } else {
+      if (!someWord.empty()) {
+        tree.add(someWord);
+        someWord.clear();
+      }
     }
-
-    std::string word;
-    while (file >> word) {
-        std::string filteredWord;
-        for (char c : word) {
-            if (std::isalpha(c)) {
-                filteredWord += std::tolower(c);
-            }
-        }
-        if (!filteredWord.empty()) {
-            tree.insert(filteredWord);
-        }
-    }
-
-    file.close();
-    return tree;
+  }
+  
+  if (!someWord.empty()) {
+    tree.add(someWord);
+  }
+  
+  file.close();
+  return tree;
 }
