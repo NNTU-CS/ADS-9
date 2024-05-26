1609c4b4afd9a45221350e55476f1eb077ba78bc
Anton <150729033+biorge@users.noreply.github.com>
2024-05-26 19:53:20 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,61 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+template<typename T>
+class BST {
+private:
+    struct Node {
+        T key;
+        int count;
+        Node* left;
+        Node* right;
+        
+        Node(T k) : key(k), count(1), left(nullptr), right(nullptr) {}
+    };
+
+    Node* root;
+
+    void insert(Node*& node, const T& key) {
+        if (!node) {
+            node = new Node(key);
+        } else if (key < node->key) {
+            insert(node->left, key);
+        } else if (key > node->key) {
+            insert(node->right, key);
+        } else {
+            node->count++;
+        }
+    }
+
+    void inorder(Node* node) const {
+        if (!node) return;
+        inorder(node->left);
+        std::cout << node->key << ": " << node->count << std::endl;
+        inorder(node->right);
+    }
+
+    void destroy(Node* node) {
+        if (node) {
+            destroy(node->left);
+            destroy(node->right);
+            delete node;
+        }
+    }
+
+public:
+    BST() : root(nullptr) {}
+    
+    ~BST() {
+        destroy(root);
+    }
+
+    void insert(const T& key) {
+        insert(root, key);
+    }
+
+    void inorder() const {
+        inorder(root);
+    }
+};
+
 #endif  // INCLUDE_BST_H_
