51dba92f2010f0c51dc5050b6b9131a9911504ee
MoklokovaKs <160238048+MoklokovaKs@users.noreply.github.com>
2024-05-26 21:26:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,27 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <locale>
+#include <cstdlib>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  BST<std::string> tree;
     std::ifstream file(filename);
-    std::string line, word;
-
-    if (file.is_open()) {
-        while (std::getline(file, line)) {
-            std::istringstream iss(line);
-            while (iss >> word) {
-                std::string lowercaseWord;
-                for (char c : word) {
-                    if (std::isalpha(c) && std::islower(c)) {
-                        lowercaseWord += c;
-                    }
-                }
-                if (!lowercaseWord.empty()) {
-                    tree.insert(lowercaseWord);
-                }
-            }
-        }
-        file.close();
+    if (!file) {
+        std::cout << "File error!!!" << std::endl;
+        return 1;
     }
-  return tree;
+    char symbol;
+    std::string word;
+    BST<std::string> tree;
+    while (file.get(symbol)) {
+        if (isalpha(symbol)) {
+            word = word + tolower(symbol);
+        } else if (!word.empty()) {
+            tree.add(word);
+            word.clear();
+        }
+    }
+    file.close();
+    return tree;
 }
