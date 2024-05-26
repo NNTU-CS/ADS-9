a5a75aa3f401d6e5b8dcf5e5694ae01b3d4079ae
yegor52 <yegorbeat2005@gmail.com>
2024-05-26 04:35:03 +0300

    АДС9

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,37 @@
 // Copyright 2021 NNTU-CS
-#include <iostream>
-#include <fstream>
-#include <locale>
-#include <cstdlib>
-#include "bst.h"
+#include  <iostream>
+#include  <fstream>
+#include  <locale>
+#include  <cstdlib>
+#include <string>
+#include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-    std::ifstream inputFile(filename);
-    if (!inputFile.is_open()) {
-        throw std::string("File not found");
+    std::ifstream input_file(filename);
+    if (!input_file.is_open()) {
+        throw std::string("ERROR: Unable to open file!");
     }
 
-    std::string currentWord = "";
-    BST<std::string> binarySearchTree;
+    std::string current_word = "";
+    BST<std::string> bst_tree;
 
-    while (inputFile >> currentWord) {
-        std::string word = "";
-        for (char symbol : currentWord) {
-            if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z')) {
-                word += tolower(symbol);
+    while (!input_file.eof()) {
+        current_word = "";
+        while (1) {
+            char symbol = input_file.get();
+            if ((symbol >= 65 && symbol <= 90) || (symbol >= 97 && symbol <= 122)) {
+                current_word += tolower(symbol);
+            } else {
+                break;
             }
         }
-        binarySearchTree.AddValue(word);
+
+        if (!current_word.empty()) { // Check for empty word
+            bst_tree.AddValue(current_word);
+        }
     }
 
-    return binarySearchTree;
+    input_file.close(); // Close the input file
+
+    return bst_tree;
 }
