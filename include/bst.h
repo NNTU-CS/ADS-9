c3cee763940ee629e05107f1267eb1b56776164a
Anton <150729033+biorge@users.noreply.github.com>
2024-05-26 20:03:04 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,8 +1,9 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-#include <iostream>
 #include <string>
+#include <iostream>
+#include <algorithm>
 
 template<typename T>
 class BST {
@@ -12,7 +13,7 @@ private:
         int count;
         Node* left;
         Node* right;
-        
+
         Node(T k) : key(k), count(1), left(nullptr), right(nullptr) {}
     };
 
@@ -45,9 +46,27 @@ private:
         }
     }
 
+    int depth(Node* node) const {
+        if (!node) return 0;
+        int left_depth = depth(node->left);
+        int right_depth = depth(node->right);
+        return std::max(left_depth, right_depth) + 1;
+    }
+
+    int search(Node* node, const T& key, int current_depth) const {
+        if (!node) return -1; // Если узел не найден
+        if (key < node->key) {
+            return search(node->left, key, current_depth + 1);
+        } else if (key > node->key) {
+            return search(node->right, key, current_depth + 1);
+        } else {
+            return current_depth;
+        }
+    }
+
 public:
     BST() : root(nullptr) {}
-    
+
     ~BST() {
         destroy(root);
     }
@@ -58,6 +77,14 @@ public:
 
     void inorder() const {
         inorder(root);
+    }
+
+    int depth() const {
+        return depth(root);
+    }
+
+    int search(const T& key) const {
+        return search(root, key, 0);
     }
 };
 
