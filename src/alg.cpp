586efa7f438ff6b03ad33c8d5536ce19651d08f5
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-05-27 20:03:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,22 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+  }
+  std::string word;
+  char s;
+  BST<std::string> three;
+  while (file.get(s)) {
+    if (isalpha(s)) {
+      word += tolower(s);
+    }
+    else {
+      three.addNew(word);
+      word.clear();
+    }
+  }
+  file.close();
+  return three;
 }
