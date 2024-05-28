161fb4d0d0ca5a982592fe0669b3b3039218786b
2malik9 <160342518+2malik9@users.noreply.github.com>
2024-05-28 19:31:31 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,5 +1,40 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
+#ifndef BST_H
+#define BST_H
+
+#include <string>
+
+template <class T>
+class BSTNode {
+public:
+    T data;
+    int count;
+    BSTNode* left;
+    BSTNode* right;
+
+    BSTNode(T value) : data(value), count(1), left(nullptr), right(nullptr) {}
+};
+
+template <class T>
+class BST {
+private:
+    BSTNode<T>* root;
+
+    void addWord(BSTNode<T>*& node, const T& value);
+
+    void inOrder(BSTNode<T>* node) const;
+
+public:
+    BST();
+
+    ~BST();
+
+    void addNode(const T& value);
+
+    void displayInOrder() const;
+};
+
 
 #endif  // INCLUDE_BST_H_
