1d926e45c182862fcf315855eb99c790748d42c4
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-05-29 22:28:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,28 +6,23 @@
 #include  <string>
 #include  "bst.h"
 
-void foo () {
-  std::string word;
-}
-
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
-//std::string word;
-foo();
-std::ifstream file(filename);
-if (!file) {
-  std::cout << "File error!" << std::endl;
-}
-char chu;
-BST<std::string> Tree;
-while (file.get(chu)) {
-  if (isalpha(chu)) {
-    word += tolower(chu);
-  } else {
-    Tree.addNew(word);
-    word.clear();
+  std::string word;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "File error!" << std::endl;
   }
-}
-file.close();
-return Tree;
+  char chu;
+  BST<std::string> Tree;
+  while (file.get(chu)) {
+    if (isalpha(chu)) {
+      word += tolower(chu);
+    } else {
+      Tree.addNew(word);
+      word.clear();
+    }
+  }
+  file.close();
+  return Tree;
 }
