73e90e9e4c20d15e466fb9a512d40ba66c171515
2malik9 <160342518+2malik9@users.noreply.github.com>
2024-06-02 22:25:50 +0300

    Update alg.cpp9

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,38 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include "BST2.h"
 
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST2<std::string> makeTree(const char* filename) {
+    BST2<std::string> tree; // Изменили тип на BST2
+    std::ifstream input_file(filename);
+
+    if (!input_file) {
+        std::cout << "Error opening file!" << std::endl;
+        return tree;
+    }
+
+    std::string line;
+    while (std::getline(input_file, line)) {
+        std::string word;
+        for (char ch : line) {
+            if (isAlpha(ch)) {
+                word += std::tolower(static_cast<unsigned char>(ch));
+            } else {
+                if (!word.empty()) {
+                    tree.insert(word);
+                    word.clear();
+                }
+            }
+        }
+
+        if (!word.empty()) {
+            tree.insert(word);
+        }
+    }
+
+    input_file.close();
+    return tree;
 }
