d2848b35164f7e8bf4d96113118a2426a313b340
Гладышев Максим <maksimgladysev731@gmail.com>
2024-06-03 23:45:46 +0300

    9

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,41 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <vector>
+
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+    }
+    BST<std::string> tree;
+    char ch;
+    char* arr = new char;
+    int counter = 0;
+    std::string stroka;
+    std::vector<std::string> vec;
+    while (!file.eof()) {
+        file.get(ch);
+        if ((ch > 64 && ch < 91) || (ch > 96 && ch < 123)) {
+            arr[counter] = tolower(ch);
+            counter++;
+        } else {
+            for (int i = 0; i < counter; i++) {
+                stroka += arr[i];
+            }
+            if (stroka != "") {
+                vec.push_back(stroka);
+            }
+            stroka = "";
+            counter = 0;
+        }
+    }
+    while (!vec.empty()) {
+        tree.add(vec.back());
+        vec.pop_back();
+    }
+    file.close();
+    return tree;
 }
