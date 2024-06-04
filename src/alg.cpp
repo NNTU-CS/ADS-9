447c77785a7801deb01375871fe646d7132d88be
2malik9 <160342518+2malik9@users.noreply.github.com>
2024-06-04 20:15:39 +0300

    Update alg.cpp9

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,9 @@
-// Copyright 2021 NNTU-CS                                                                                                                                                       #include <iostream>
+// Copyright 2021 NNTU-CS                                                                                                                                                       #include <iostream>                                                                                                                                                  #include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include "BST.h"
+
 bool isAlpha(char ch) {
     return std::isalpha(static_cast<unsigned char>(ch));
 }
