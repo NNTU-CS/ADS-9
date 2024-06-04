a026962ae6d2561a1d4dd12d8454491d25db4c99
2malik9 <160342518+2malik9@users.noreply.github.com>
2024-06-04 20:13:35 +0300

    Update alg.cpp9

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,36 +1,36 @@
 // Copyright 2021 NNTU-CS                                                                                                                                                       #include <iostream>
-#include <fstream>
-#include <string>
-#include <cctype>
-#include "BST.h"
-
 bool isAlpha(char ch) {
-  return std::isalpha(static_cast<unsigned char>(ch));
+    return std::isalpha(static_cast<unsigned char>(ch));
 }
+
 BST<std::string> makeTree(const char* filename) {
-  BST<std::string> tree;
-  std::ifstream input_file(filename);
-  if (!input_file) {
-    std::cout << "Error opening file!" << std::endl;
-    return tree;
-  }
-  std::string line;
-  while (std::getline(input_file, line)) {
-    std::string word;
-    for (char ch : line) {
-      if (isAlpha(ch)) {
-        word += std::tolower(static_cast<unsigned char>(ch));
-      } else {
-        if (!word.empty()) {
-          tree.insert(word);
-          word.clear();
+    BST<std::string> tree;
+    std::ifstream input_file(filename);
+
+    if (!input_file) {
+        std::cout << "Error opening file!" << std::endl;
+        return tree;
+    }
+
+    std::string line;
+    while (std::getline(input_file, line)) {
+        std::string word;
+        for (char ch : line) {
+            if (isAlpha(ch)) {
+                word += std::tolower(static_cast<unsigned char>(ch));
+            } else {
+                if (!word.empty()) {
+                    tree.insert(word);
+                    word.clear();
+                }
+            }
         }
-      }
+
+        if (!word.empty()) {
+            tree.insert(word);
+        }
     }
-    if (!word.empty()) {
-      tree.insert(word);
-    }
-  }
-  input_file.close();
-  return tree;
+
+    input_file.close();
+    return tree;
 }
