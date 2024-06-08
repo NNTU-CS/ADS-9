3fcc8f276c369b53915d616ea67dc5ebea49b984
Евгений <160422749+MAKARENNA110@users.noreply.github.com>
2024-06-08 17:48:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,30 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+char ChangeRegistr(char ch) {
+    if (ch >= 'A' && ch <= 'Z') {
+        ch += ('a' - 'A');
+    }
+    return ch;
+}
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    BST<std::string> bst;
+    std::string word;
+    bool flag = false;
+    while (!file.eof()) {
+        char ch = file.get();
+        ch = ChangeRegistr(ch);
+        if (ch >= 'a' && ch <= 'z' && !flag) {
+            flag = true;
+            word = ch;
+        } else if (ch >= 'a' && ch <= 'z' && flag) {
+            word += ch;
+        } else {
+            flag = false;
+            bst.add(word);
+        }
+    }
+    file.close();
+    return bst;
 }
