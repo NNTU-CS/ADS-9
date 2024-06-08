131d041f51c42ada2afd79b32f79b15e5f18c18d
laal28 <129623228+laal28@users.noreply.github.com>
2024-06-08 16:59:13 +0300

    added debug print

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,24 +5,24 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-BST<std::string> makeTree(const char* filename) {
+BST<std::string> makeTree( const char* filename ) {
     BST<std::string> tree;
-    std::fstream input(filename);
+    std::fstream input( filename );
     std::string word = "";
 
     char follow;
-    for (; input; follow = input.get()) {
+    for ( ; input; follow = input.get() ) {
         if (isalpha(follow)) {
             follow = tolower(follow);
             word += follow;
         } else if (!(word.empty())) {
             tree.add(word);
+            std::cout << word << std::endl;
             word = "";
         }
     }
 
-
-    if (!(word.empty()))
+    if ( !( word.empty() ) )
         tree.add(word);
 
     input.close();
