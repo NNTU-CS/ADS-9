9051f73ac3472cd5f3e804515dd01b4df7fdcc63
SfDarkFire <160343903+SfDarkFire@users.noreply.github.com>
2024-06-09 18:02:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,26 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> WarPeaсeTree;
+  std::ifstream file(filename);
+  std::string Word;
+  char Token;
+  while (!file.eof()) {
+    Token = file.get();
+    Token = LowercaseTranslation(Token);
+    if (Token >= 'a' && Token <= 'z') {
+      Word += Token;
+    } else {
+      WarPeaсeTree.add(Word);
+      Word.clear();
+    }
+  }
+  return WarPeaсeTree;
+}
+
+char LowercaseTranslation(char Token) {
+  if (Token >= 'A' && Token <= 'Z') {
+    Token = Token + ('a' - 'A');
+  }
+  return Token;
 }
