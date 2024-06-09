60232bb089165dee8b64e205472447e1c74881a1
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-06-09 20:42:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,31 +5,31 @@
 #include  <cstdlib>
 #include  "bst.h"
 
-bool isLatinLetter(char ch) {
-    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+char toLowerChar(char ch) {
+    return std::tolower(ch, std::locale::classic());
 }
+
+bool isAlphaChar(char ch) {
+    return std::isalpha(ch, std::locale::classic());
+}
+
 BST<std::string> makeTree(const char* filename) {
     BST<std::string> tree;
     std::ifstream file(filename);
     if (!file) {
         std::cerr << "File error!" << std::endl;
-        return tree;
+        exit(EXIT_FAILURE);
     }
     std::string word;
-    char ch;
-    while (file.get(ch)) {
-        if (isLatinLetter(ch)) {
-            word += std::tolower(ch, std::locale());
-        } else if (!word.empty()) {
-            tree.insert(word);
-            word.clear();
+    while (file >> word) {
+        std::string filteredWord;
+        std::remove_copy_if(word.begin(), word.end(), std::back_inserter(filteredWord),
+                            [](char c { return !isAlphaChar(c); });
+        std::transform(filteredWord.begin(), filteredWord.end(), filteredWord.begin(), toLowerChar);
+        if (!filteredWord.empty()) {
+            tree.insert(filteredWord);
         }
     }
-
-    if (!word.empty()) {
-        tree.insert(word);
-    }
-
     file.close();
     return tree;
 }
