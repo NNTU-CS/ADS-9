cf43f9f41055d071552b0f65af9540eed1728b58
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-06-09 21:16:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,8 +26,7 @@ BST<std::string> makeTree(const char* filename) {
         word.erase(std::remove_if(
             word.begin(),
             word.end(),
-            [](char c) { return !isAlphaChar(c); },
-            word.end()));
+            [](char c) { return !isAlphaChar(c);}));
         std::string filteredWord;
         std::transform(word.begin(), word.end(), std::back_inserter(filteredWord), toLowerChar);
         if (!filteredWord.empty()) {
